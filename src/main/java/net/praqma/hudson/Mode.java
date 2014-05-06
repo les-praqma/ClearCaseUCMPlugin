b1141ec80701fcc9187eaafa8a1b87a11128241e
Leif Sorensen <les@praqma.net>
2014-05-06 08:12:15 +0200

    test

--- src/main/java/net/praqma/hudson/Mode.java
+++ src/main/java/net/praqma/hudson/Mode.java
@@ -4,7 +4,6 @@ import hudson.FilePath;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.TaskListener;
-import hudson.scm.PollingResult;
 
 import java.io.File;
 
